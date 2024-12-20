8a699054d200ea08bce2ccf2e26408ac66cd60f3
Anshul Goel <anshulgoel27@gmail.com>
2024-12-20 15:53:48 +0530

    updated for nats

--- client.go
+++ client.go
@@ -1,32 +1,23 @@
 package pubsub
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
-
-	"gocloud.dev/pubsub"
-	_ "gocloud.dev/pubsub/awssnssqs"
-	_ "gocloud.dev/pubsub/azuresb"
-	_ "gocloud.dev/pubsub/gcppubsub"
-	_ "gocloud.dev/pubsub/kafkapubsub"
-	_ "gocloud.dev/pubsub/natspubsub"
-	_ "gocloud.dev/pubsub/rabbitpubsub"
+	"os"
 
 	"github.com/luraproject/lura/v2/config"
 	"github.com/luraproject/lura/v2/logging"
 	"github.com/luraproject/lura/v2/proxy"
+	"github.com/nats-io/nats.go"
 )
 
-var OpenCensusViews = pubsub.OpenCensusViews
 var errNoBackendHostDefined = fmt.Errorf("no host backend defined")
 
 const (
-	publisherNamespace  = "github.com/devopsfaith/krakend-pubsub/publisher"
-	subscriberNamespace = "github.com/devopsfaith/krakend-pubsub/subscriber"
+	publisherNamespace = "github_com/anshulgoel27/nats-publisher"
 )
 
 func NewBackendFactory(ctx context.Context, logger logging.Logger, bf proxy.BackendFactory) *BackendFactory {
@@ -44,10 +35,6 @@ type BackendFactory struct {
 }
 
 func (f *BackendFactory) New(remote *config.Backend) proxy.Proxy {
-	if prxy, err := f.initSubscriber(f.ctx, remote); err == nil {
-		return prxy
-	}
-
 	if prxy, err := f.initPublisher(f.ctx, remote); err == nil {
 		return prxy
 	}
@@ -60,7 +47,6 @@ func (f *BackendFactory) initPublisher(ctx context.Context, remote *config.Backe
 		return proxy.NoopProxy, errNoBackendHostDefined
 	}
 
-	dns := remote.Host[0]
 	cfg := &publisherCfg{}
 
 	if err := getConfig(remote, publisherNamespace, cfg); err != nil {
@@ -70,101 +56,80 @@ func (f *BackendFactory) initPublisher(ctx context.Context, remote *config.Backe
 		return proxy.NoopProxy, err
 	}
 
-	logPrefix := "[BACKEND: " + dns + cfg.TopicURL + "][PubSub]"
-	t, err := pubsub.OpenTopic(ctx, dns+cfg.TopicURL)
+	logPrefix := "[BACKEND: " + cfg.TopicURL + "][PubSub]"
+	url := os.Getenv("NATS_URL")
+	if url == "" {
+		url = nats.DefaultURL
+	}
+
+	// Connect to NATS server
+	nc, err := nats.Connect(url)
 	if err != nil {
-		f.logger.Error(fmt.Sprintf(logPrefix, err.Error()))
+		f.logger.Error(fmt.Sprintf("%s Error connecting to NATS: %s", logPrefix, err.Error()))
 		return proxy.NoopProxy, err
 	}
 
-	f.logger.Debug(logPrefix, "Publisher initialized sucessfully")
+	// Create a JetStream context
+	js, err := nc.JetStream()
+	if err != nil {
+		f.logger.Error(fmt.Sprintf("%s Error initializing JetStream: %s", logPrefix, err.Error()))
+		return proxy.NoopProxy, err
+	}
 
+	// Ensure the stream exists
+	streamConfig := &nats.StreamConfig{
+		Name:     cfg.StreamName,
+		Subjects: []string{cfg.TopicURL},
+	}
+	if _, err := js.AddStream(streamConfig); err != nil && !errors.Is(err, nats.ErrStreamNameAlreadyInUse) {
+		f.logger.Error(fmt.Sprintf("%s Error adding stream: %s", logPrefix, err.Error()))
+		return proxy.NoopProxy, err
+	}
+
+	f.logger.Debug(fmt.Sprintf("%s Publisher initialized successfully", logPrefix))
+
+	// Handle graceful shutdown
 	go func() {
 		<-ctx.Done()
-		t.Shutdown(context.Background())
+		nc.Drain()
 	}()
 
 	return func(ctx context.Context, r *proxy.Request) (*proxy.Response, error) {
-		body, err := ioutil.ReadAll(r.Body)
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			return nil, err
 		}
+
 		headers := map[string]string{}
 		for k, vs := range r.Headers {
 			headers[k] = vs[0]
 		}
-		msg := &pubsub.Message{
-			Metadata: headers,
-			Body:     body,
+
+		// Convert headers into NATS message headers
+		msgHeaders := nats.Header{}
+		for k, v := range headers {
+			msgHeaders.Set(k, v)
 		}
 
-		if err := t.Send(ctx, msg); err != nil {
+		// Publish the message to the topic
+		msg := &nats.Msg{
+			Subject: cfg.TopicURL,
+			Header:  msgHeaders,
+			Data:    body,
+		}
+
+		if _, err := js.PublishMsg(msg); err != nil {
+			f.logger.Error(fmt.Sprintf("%s Error publishing message: %s", logPrefix, err.Error()))
 			return nil, err
 		}
+
 		return &proxy.Response{IsComplete: true}, nil
 	}, nil
 }
 
-func (f *BackendFactory) initSubscriber(ctx context.Context, remote *config.Backend) (proxy.Proxy, error) {
-	if len(remote.Host) < 1 {
-		return proxy.NoopProxy, errNoBackendHostDefined
-	}
-
-	dns := remote.Host[0]
-	cfg := &subscriberCfg{}
-
-	if err := getConfig(remote, subscriberNamespace, cfg); err != nil {
-		if _, ok := err.(*NamespaceNotFoundErr); !ok {
-			f.logger.Error(fmt.Sprintf("[BACKEND][PubSub] Error initializing subscriber: %s", err.Error()))
-		}
-		return proxy.NoopProxy, err
-	}
-
-	topicURL := dns + cfg.SubscriptionURL
-	logPrefix := "[BACKEND: " + topicURL + "][PubSub]"
-
-	sub, err := pubsub.OpenSubscription(ctx, topicURL)
-	if err != nil {
-		f.logger.Error(fmt.Sprintf(logPrefix, "Error while opening subscription: %s", err.Error()))
-		return proxy.NoopProxy, err
-	}
-
-	f.logger.Debug(logPrefix, "Subscriber initialized sucessfully")
-
-	go func() {
-		<-ctx.Done()
-		sub.Shutdown(context.Background())
-	}()
-
-	ef := proxy.NewEntityFormatter(remote)
-
-	return func(ctx context.Context, _ *proxy.Request) (*proxy.Response, error) {
-		msg, err := sub.Receive(ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		var data map[string]interface{}
-		if err := remote.Decoder(bytes.NewBuffer(msg.Body), &data); err != nil && err != io.EOF {
-			// TODO: figure out how to Nack if possible
-			// msg.Nack()
-			return nil, err
-		}
-
-		msg.Ack()
-
-		newResponse := proxy.Response{Data: data, IsComplete: true}
-		newResponse = ef.Format(newResponse)
-		return &newResponse, nil
-	}, nil
-}
-
 type publisherCfg struct {
-	TopicURL string `json:"topic_url"`
-}
-
-type subscriberCfg struct {
-	SubscriptionURL string `json:"subscription_url"`
+	TopicURL   string `json:"topic_url"`
+	StreamName string `json:"stream_name"`
 }
 
 func getConfig(remote *config.Backend, namespace string, v interface{}) error {
